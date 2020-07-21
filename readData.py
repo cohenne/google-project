baf9e3d08f4c42c8c5f7bdf7c9f14acec4d3dfe2
nechama cohen <cn@sem-wolf.org>
2020-07-21 22:50:55 +0300

    add read from file

--- readData.py
+++ readData.py
@@ -1,9 +1,13 @@
 import re
 from collections import defaultdict
 from AutoCompleteData import AutoCompleteData
-# import os
+from pathlib import Path
 
-words = [AutoCompleteData("hello world", ""), AutoCompleteData("nice world", ""), AutoCompleteData("wow beautiful", ""), AutoCompleteData("I am", ""), AutoCompleteData("hi", ""), AutoCompleteData("hii", "")]
+words = []
+
+sentence_index = 0
+
+data_dict = defaultdict(list)
 
 
 def find_sequence(string):
@@ -19,28 +23,30 @@ def all_sub_words(line):
     return [line[i: j] for i in range(len(line)) for j in range(i + 1, len(line) + 1)]
 
 
-data_dict = defaultdict(list)
-
-
-# def read_data(file_name):
-#     x_file = open(file_name, "r")
-#     x_line = x_file.readlines()
-#     for i in range(len(x_line)):
-#         line = format_line(words[i])
-#         sub_words = all_sub_words(line)
-#         for word in sub_words:
-#             data_dict[word].append(i)
+def read_data(file_name):
+    x_file = open(file_name, "r")
+    x_line = x_file.readlines()
+    for line in x_line:
+        line = format_line(line)
+        sub_words = all_sub_words(line)
+        for word in sub_words:
+            global sentence_index
+            if sentence_index not in data_dict[word]:
+                data_dict[word].append(sentence_index)
+        sentence_index += 1
+        words.append(AutoCompleteData(line, file_name))
 
 
 def init():
-    # entries = os.listdir()
-    # pass
-    #
-    for i in range(len(words)):
-        line = format_line(words[i].get_complete_sentence())
-        sub_words = all_sub_words(line)
-        for word in sub_words:
-            data_dict[word].append(i)
+
+    directory_list = ["c-api"]
+    while len(directory_list) != 0:
+        basepath = Path(directory_list.pop(-1))
+        for entry in basepath.iterdir():
+            if entry.is_dir():
+                directory_list.append(entry)
+            else:
+                read_data(entry)
 
 
 if __name__ == '__main__':
@@ -48,12 +54,14 @@ if __name__ == '__main__':
     print("Loading the file and preparing the system....")
     init()
     x = input("The system is ready. Enter your text:")
-    while x:
 
+    while x:
         print("There are 5 suggestions")
         suggestions = find_sequence(x)
+
         for i in range(len(suggestions)):
-            print(f'{i+1}. {suggestions[i].get_complete_sentence()}')
+            print(f'{i+1}. {suggestions[i].get_complete_sentence()}', end='')
+
         if x[-1] != '#':
             print(x, end='')
             x += input()
