5fbe32f29cce24ec776719a922a4f6098609757c
nechama cohen <cn@sem-wolf.org>
2020-07-22 03:02:06 +0300

    add function to find data to create autoCompleteData obj

--- readData.py
+++ readData.py
@@ -1,18 +1,23 @@
 import re
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from AutoCompleteData import AutoCompleteData
 from pathlib import Path
 
-words = []
 
-sentence_index = 0
-
+sentences = {}
 data_dict = defaultdict(list)
+subString = namedtuple('subString', ['id', 'offset'])
+sentence_path = namedtuple('sentence_url', ['sentence', 'path'])
+sentences_index = 0
 
 
 def find_sequence(string):
     indexes = data_dict[string][:5]
-    return [words[i] for i in indexes]
+    return [AutoCompleteData(sentences[index.id].sentence, sentences[index.id].path, index.offset, get_score(sentences[index.id].sentence, string)) for index in indexes]
+
+
+def get_score(sentences, string, decrease=0):
+    return len(string)*2 - decrease
 
 
 def format_line(line):
@@ -22,19 +27,31 @@ def format_line(line):
 def all_sub_words(line):
     return [line[i: j] for i in range(len(line)) for j in range(i + 1, len(line) + 1)]
 
+# are there data file directory out of our project ?
+# def find_path(file_name):
+#     # return os.path.abspath(file_name)
+#     for root, dirs, files in os.walk(r"C:\Users\Lenovo\Documents\google_project\c-api"):
+#         for name in files:
+#             if name == str(file_name) + ".txt":
+#                 return os.path.abspath(os.path.join(root, name))
+#     return False
+
 
 def read_data(file_name):
     x_file = open(file_name, "r")
-    x_line = x_file.readlines()
+    x_line = x_file.read().splitlines()
+    global sentences_index
+
     for line in x_line:
         line = format_line(line)
         sub_words = all_sub_words(line)
+
         for word in sub_words:
-            global sentence_index
-            if sentence_index not in data_dict[word]:
-                data_dict[word].append(sentence_index)
-        sentence_index += 1
-        words.append(AutoCompleteData(line, file_name))
+            if sentences_index not in data_dict[word]:
+                data_dict[word].append(subString(sentences_index, line.index(word)))
+
+        sentences[sentences_index] = sentence_path(line, file_name)
+        sentences_index += 1
 
 
 def init():
@@ -46,6 +63,7 @@ def init():
             if entry.is_dir():
                 directory_list.append(entry)
             else:
+                print(entry)
                 read_data(entry)
 
 
@@ -56,17 +74,22 @@ if __name__ == '__main__':
     x = input("The system is ready. Enter your text:")
 
     while x:
-        print("There are 5 suggestions")
-        suggestions = find_sequence(x)
-
-        for i in range(len(suggestions)):
-            print(f'{i+1}. {suggestions[i].get_complete_sentence()}', end='')
-
         if x[-1] != '#':
+            suggestions = find_sequence(x)
+
+            if suggestions:
+                print(f"There are {len(suggestions)} suggestions")
+                for i in range(len(suggestions)):
+                    print(f'{i+1}. {suggestions[i].get_complete_sentence()} , path = {suggestions[i].get_source_text()}')
+
+            else:
+                print("There are'nt suggestions")
+
             print(x, end='')
             x += input()
+
         else:
-            x = input()
+            x = input("Enter your text:")
 
 
 
